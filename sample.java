c6b16bd61419e6e9ed48c359ff07477de0d3fdd9
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-17 10:29:44 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -29,3 +29,49 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            intersect(nums2, nums1);
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> result = new ArrayList<>();
+        int low=0;
+        int high=n-1;
+        int bindex=-1;
+        for (int i=0;i<m;i++) {
+            bindex = binarysearch(nums2, low, high, nums1[i]);
+            if (bindex != -1) {
+                result.add(nums1[i]);
+                low = bindex+1;
+            }
+        }
+        int[] answer = new int[result.size()];
+        for (int i=0;i<result.size();i++) {
+            answer[i] = result.get(i);    
+        }
+        return answer;
+    }
+    private int binarysearch(int[] nums, int low, int high, int target) {
+        while (low <=high) {
+            int mid= low + (high-low)/2;
+            if (nums[mid] == target) {
+                if (mid == low || nums[mid-1] != target) {
+                    return mid;
+                }
+                high = mid-1;
+            } else if (nums[mid] < target) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
